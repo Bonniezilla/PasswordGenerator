7170ef5b86c2284d2837bad46d98ba4f50740c23
Bonniezilla <bonnieevil86@gmail.com>
2023-04-20 20:43:36 -0300

    Melhorando o Css e Js, com função de copiar senha

--- default.js
+++ default.js
@@ -1,7 +1,7 @@
-const buttonEl = document.getElementById('generate-btn')
-const passwordDivEl = document.getElementById('passwordDiv')
-const inputEl = document.getElementById('input-el')
-const errorP = document.getElementById('error-p')
+const formEl = document.querySelector('form');
+const passwordsEl = document.querySelectorAll('.password');
+const inputEl = document.getElementById('input-el');
+const errorP = document.getElementById('errorP');
 
 const allChars = [
     'A', 'B', 'C', 'D', 'F', 'G', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -10,57 +10,42 @@ const allChars = [
     '!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '=', '+'
 ]
 
-let password = "";
-
-let passwords = []
-
-buttonEl.onclick = () => {
-    if (inputEl.value > 3 && inputEl.value < 51) {
-        errorP.textContent = ''
-        return createPasswords(),
-            getNewPassword()
-    } else if (inputEl.value < 4) {
-        errorP.textContent = 'Insira um número maior que 3*'
+formEl.onsubmit = (e) => {
+    e.preventDefault();
+    if (inputEl.value >= 5 && inputEl.value <= 35) {
+        errorP.style.display = "none";
+        renderPassword();
     } else {
-        errorP.textContent = 'Insira um número menor 50*'
+        errorP.style.display = "flex"
     }
+}
+
+function copy(password) {
+    alert(`Copiado com sucesso ${passwordsEl[password].value}`);
+    passwordsEl[password].select();
+    navigator.clipboard.writeText(passwordsEl[password].value);
 }
 
 function getRandomChar() {
     return allChars[Math.floor(Math.random() * allChars.length)]
 }
 
-function createPasswords() {
-    let passwordCreationArray = [""]
-    if (inputEl.value >= 4) {
-        for (let i = 0; i <= inputEl.value; i++) {
+function createPassword(value = inputEl.value) {
+    let password = "";
+    let passwordCreationArray = [""];
+    if (value >= 4) {
+        for (let i = 0; i <= value; i++) {
             passwordCreationArray.push(getRandomChar())
-            password += passwordCreationArray[i]
+            password += passwordCreationArray[i];
         }
     }
-    return password
-}
-
-
-function getNewPassword() {
-    passwords = []
-    password = ''
-    let newPassword = ''
-    for (let i = 0; i < 5; i++) {
-        newPassword = createPasswords()
-        passwords.push(newPassword)
-        password = ''
-    }
-    return createPasswords(),
-        renderPassword()
+    return password;
 }
 
 function renderPassword() {
-    passwordDivEl.firstChild.textContent = passwords[0]
-    passwordDivEl.lastChild.textContent = passwords[3]
-
-    passwordDivEl.firstChild.style.color = 'rgb(56, 225, 169)'
-    passwordDivEl.firstChild.style.fontSize = '15px'
-    passwordDivEl.lastChild.style.fontSize = '15px'
-    passwordDivEl.lastChild = 'rgb(56, 225, 169)'
+    for (let i = 0; i < 4; i++) {
+        passwordsEl[i].value = createPassword();
+        passwordsEl[i].title = "Clique para copiar para a área de transferencia";
+        passwordsEl[i].style.cursor = "pointer;"
+    }
 }
