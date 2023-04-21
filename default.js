36a0789cdbfa564071fb1d3f17a603928c394f72
Bonniezilla <bonnieevil86@gmail.com>
2023-04-21 11:10:57 -0300

    Adding copy function and improving the code

--- default.js
+++ default.js
@@ -1,15 +1,19 @@
+//All necessary HTML elements
 const formEl = document.querySelector('form');
-const passwordsEl = document.querySelectorAll('.password');
+const passwordsEl = document.querySelector('.passwordDiv');
 const inputEl = document.getElementById('input-el');
 const errorP = document.getElementById('errorP');
 
+// All available characters
 const allChars = [
-    'A', 'B', 'C', 'D', 'F', 'G', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '=', '+'
-]
+    '!', '"', '#',  '$',  '%',  '&' ,"'", '(', ')', '*' ,
+    '+', ',', '-', '.', '/', ':', ';' ,'<', '=', '>', '?', 
+    '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'    
+];
 
+// Function on submit to generate and render passwords
 formEl.onsubmit = (e) => {
     e.preventDefault();
     if (inputEl.value >= 5 && inputEl.value <= 35) {
@@ -20,16 +24,30 @@ formEl.onsubmit = (e) => {
     }
 }
 
+// Function on click to require copy() function
+passwordsEl.onclick = (e) => {
+    if(e.target != passwordsEl && e.target.value != "") {
+        copy(e.target);
+    }
+}
+
+// Copy function
 function copy(password) {
-    alert(`Copiado com sucesso ${passwordsEl[password].value}`);
-    passwordsEl[password].select();
-    navigator.clipboard.writeText(passwordsEl[password].value);
+    alert(`Copiado com sucesso ${password.value}`);
+    password.select();
+    navigator.clipboard.writeText(password.value);
 }
 
+// Function to retunr a random char
 function getRandomChar() {
-    return allChars[Math.floor(Math.random() * allChars.length)]
+    let randomChar = allChars[Math.floor(Math.random() * allChars.length)];
+    if (Boolean(Math.random() < 0.5)) {
+        randomChar = randomChar.toUpperCase();
+    }
+    return randomChar;
 }
 
+// Function to generate a single random password and return
 function createPassword(value = inputEl.value) {
     let password = "";
     let passwordCreationArray = [""];
@@ -42,10 +60,11 @@ function createPassword(value = inputEl.value) {
     return password;
 }
 
+// Function to render the passwords
 function renderPassword() {
-    for (let i = 0; i < 4; i++) {
-        passwordsEl[i].value = createPassword();
-        passwordsEl[i].title = "Clique para copiar para a área de transferencia";
-        passwordsEl[i].style.cursor = "pointer;"
+    for (let i = 0; i < passwordsEl.children.length; i++) {
+        passwordsEl.children.item(i).value = createPassword();
+        passwordsEl.children.item(i).title = "Clique para copiar para a área de transferencia";
+        passwordsEl.children.item(i).style.cursor = "pointer;"
     }
 }
